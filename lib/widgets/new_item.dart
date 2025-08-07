690c997146817e4d744c2ce84264eed5c7baf79c
Alex-Yu21 <yualex2112@gmail.com>
2025-08-07 14:21:18 +0200

    add forms in NewItem

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -14,7 +15,56 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(title: Text('Add a new item ')),
       body: Padding(
         padding: EdgeInsets.all(12),
-        child: Column(children: [Text('sss')]),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                maxLength: 50,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Quantity'),
+                      initialValue: '1',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.key,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
